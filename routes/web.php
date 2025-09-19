7103a7a10440164a011c76963f23d3396edde319
gitcore33 <erickestanislau25@gmail.com>
2025-09-18 21:37:40 -0300

    creates first routes

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,14 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+// Tattoos page
+Route::get('/tattoos', function () {
+    return view('tattoos');
+});
+
+// Clothes page
+Route::get('/clothes', function () {
+    return view('clothes');
 });
